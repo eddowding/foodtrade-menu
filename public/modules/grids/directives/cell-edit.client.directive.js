81d92a0d110842b76b024d596370bc599c823cbf
Jaideep Das <jaideep.jdof@gmail.com>
2015-05-02 02:37:01 +0530

    added popover form

---
+++ public/modules/grids/directives/cell-edit.client.directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('grids').directive('cellEdit', ['$compile',
+	function($compile) {
+		return {
+			templateUrl: 'modules/grids/views/cell-edit.client.partial.html',
+			restrict: 'E',
+            scope: {
+              rowNumber: '=rowNumber',
+              columnName: '=columnName'
+            },
+			link: function postLink(scope, element, attrs) {
+              scope.cell = scope.$parent.grid.tableData[scope.rowNumber][scope.columnName];
+              $(element).popover({
+                html: true,
+                title: 'Cell edit',
+                content: function() {
+                  return $compile($('.cell-edit-form').html())(scope);
+                }
+              });
+			}
+		};
+	}
+]);
