03aa4dc7b018ab49e1bc1e53cd3ad9094c07f7ed
Jaideep Das <jaideep.jdof@gmail.com>
2015-05-15 01:00:42 +0530

    signup new form

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,17 +1,33 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
+angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication', 'Accounts', 'Establishments',
+	function($scope, $http, $location, Authentication, Accounts, Establishments) {
 		$scope.authentication = Authentication;
 
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
 
+        $scope.account = {};
+        $scope.establishment = {};
+
 		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
+            $scope.user.username = $scope.user.email;
+			$http.post('/auth/signup', $scope.user).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
+                $scope.account.user = response._id;
+                Accounts.save($scope.account)
+                .$promise
+                .then(function(account) {
+                  $scope.account = account;
+                });
+                $scope.establishment.user = response._id;
+                Establishments.save($scope.establishment)
+                .$promise
+                .then(function(establishment) {
+                  $scope.establishment = establishment;
+                });
 				// And redirect to the listing view
 				$location.path('/grids');
 			}).error(function(response) {
@@ -20,10 +36,9 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		};
 
 		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
+			$http.post('/auth/signin', $scope.user).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
-
 				// And redirect to the listing view
 				$location.path('/grids');
 			}).error(function(response) {
