b995226eeeb2fdd98f26ab7bc48f436912894faf
Jaideep Das <jaideep.jdof@gmail.com>
2015-05-22 14:35:50 +0530

    added user dashboard

--- app/controllers/grids.server.controller.js
+++ app/controllers/grids.server.controller.js
@@ -73,7 +73,7 @@ exports.delete = function(req, res) {
  * List of Grids
  */
 exports.list = function(req, res) {
-  Grid.find().sort('-created').populate('user', 'displayName').exec(function(err, grids) {
+  Grid.find().sort('-created').populate('user').exec(function(err, grids) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
--- public/modules/ftm/controllers/dashboard.client.controller.js
+++ public/modules/ftm/controllers/dashboard.client.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
-angular.module('ftm').controller('DashboardController', ['$scope',
-	function($scope) {
-		// Controller Logic
-		// ...
+angular.module('ftm').controller('DashboardController', ['$scope', 'Grids', 'Authentication',
+	function($scope, Grids, Authentication) {
+        $scope.authentication = Authentication;
+        
+        Grids.query({user: $scope.authentication.user._id})
+        .$promise
+        .then(function(grids) {
+            $scope.grids = grids;
+        });
 	}
 ]);
