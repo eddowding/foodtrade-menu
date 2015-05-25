b4ddcad09ccb1a6620eb013d5615c6e59bf1fc66
Jaideep Das <jaideep.jdof@gmail.com>
2015-05-26 01:36:28 +0530

    git commit added grid detail view from dashboard

--- public/modules/ftm/config/ftm.client.routes.js
+++ public/modules/ftm/config/ftm.client.routes.js
@@ -5,6 +5,10 @@ angular.module('ftm').config(['$stateProvider',
 	function($stateProvider) {
 		// Ftm state routing
 		$stateProvider.
+		state('grid-detail', {
+			url: '/grid-detail/:gridId',
+			templateUrl: 'modules/ftm/views/grid-detail.client.view.html'
+		}).
 		state('dashboard', {
 			url: '/dashboard',
 			templateUrl: 'modules/ftm/views/dashboard.client.view.html'
