148b7e862b920595a106e5fb4343369ab0166773
Jaideep Das <jaideep.jdof@gmail.com>
2015-05-31 18:40:06 +0530

    cascade logic wip

--- public/modules/ftm/controllers/grid-detail.client.controller.js
+++ public/modules/ftm/controllers/grid-detail.client.controller.js
@@ -1,223 +1,267 @@
 'use strict';
 
 angular.module('ftm').controller('GridDetailController', ['$scope', 'Authentication', '$stateParams', 'Grids', '$location',
-	function($scope, Authentication, $stateParams, Grids, $location) {
-		$scope.shareUrl = $location.absUrl();
-		$scope.authentication = Authentication;
+  function($scope, Authentication, $stateParams, Grids, $location) {
+    $scope.shareUrl = $location.absUrl();
+    $scope.authentication = Authentication;
 
-		// Find existing Grid
-		$scope.findOne = function() {
-			$scope.grid = Grids.get({
-				gridId: $stateParams.gridId
-			});
-		};
-
-
-				$scope.gridColumns = [
-					'nut',
-					'peanut',
-					'sesameSeed',
-					'fish',
-					'crustacean',
-					'mollusc',
-					'egg',
-					'milk',
-					'cereal',
-					'lupin',
-					'celery',
-					'mustard',
-					'soya',
-					'sulphurDioxide',
-					'other'
-				];
+    // Find existing Grid
+    $scope.findOne = function() {
+      $scope.grid = Grids.get({
+        gridId: $stateParams.gridId
+      });
+    };
 
 
-				$scope.placeholderRow = {
-					item: {},
-					veggie: {type: null},
-				};
+    $scope.gridColumns = [
+      'nut',
+      'peanut',
+      'sesameSeed',
+      'fish',
+      'crustacean',
+      'mollusc',
+      'egg',
+      'milk',
+      'cereal',
+      'lupin',
+      'celery',
+      'mustard',
+      'soya',
+      'sulphurDioxide',
+      'other'
+    ];
 
-				$scope.gridColumns.forEach(function(column, index) {
-					$scope.placeholderRow[column] = {allergenType: 'No allergen'};
-				});
 
-				$scope.resetRow = angular.copy($scope.placeholderRow);
+    $scope.placeholderRow = {
+      item: {},
+      veggie: {
+        type: null
+      },
+    };
 
-				$scope.updatePlaceholderRowCellFn = function(columnName, cell) {
-					$scope.placeholderRow[columnName] = cell;
-				};
+    $scope.gridColumns.forEach(function(column, index) {
+      $scope.placeholderRow[column] = {
+        allergenType: 'No allergen'
+      };
+    });
 
-				$scope.saveGridFn = function() {
-					var tmpGrid = angular.copy($scope.grid);
-					try {
-			tmpGrid.updated = new Date();
-						tmpGrid.$save();
-						console.info('Saving grid');
-					} catch(err) {
-						console.error(err);
-					}
-				};
+    $scope.resetRow = angular.copy($scope.placeholderRow);
 
-				$scope.updateTableDataRowCellFn = function(columnName, rowNumber, cell) {
-					$scope.grid.tableData[rowNumber][columnName] = cell;
-					$scope.saveGridFn();
-				};
+    $scope.updatePlaceholderRowCellFn = function(columnName, cell) {
+      $scope.placeholderRow[columnName] = cell;
+    };
 
-				$scope.newRowLogicFn = function() {
-					if (!$scope.placeholderRow.item.name) {
-						return;
-					}
-						var lineSplit = $scope.placeholderRow.item.name.split('\n');
-						lineSplit.forEach(function(line, index) {
-							line = line.trim();
-							if (line.length == 0) {
-								return;
-							}
-							if (line.charAt(line.length - 1) == ':') {
-								$scope.placeholderRow.item.type = 1;
-							} else if (line.charAt(0) == '-') {
-								line = line.substring(1, line.length);
-								$scope.placeholderRow.item.type = 3;
-							} else if (line.charAt(0) == '*') {
-								line = line.substring(1, line.length);
-								$scope.placeholderRow.item.type = 3;
+    $scope.saveGridFn = function() {
+      var tmpGrid = angular.copy($scope.grid);
+      try {
+        tmpGrid.updated = new Date();
+        tmpGrid.$save();
+        console.info('Saving grid');
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    $scope.updateTableDataRowCellFn = function(columnName, rowNumber, cell) {
+      $scope.grid.tableData[rowNumber][columnName] = cell;
+      $scope.saveGridFn();
+    };
+
+    $scope.updateSectionsFn = function() {
+      var columnValues = null;
+      for (var i = $scope.grid.tableData.length - 1; i >= 0; i--) {
+        var row = $scope.grid.tableData[i];
+        if (row.item.type == 1) {
+          if (columnValues) {
+            for (var col in columnValues) {
+							if (columnValues[col].indexOf('Fixed') > -1) {
+								row[col].allergenType = 'Fixed';
+							} else if (columnValues[col].indexOf('Removable') > -1) {
+								row[col].allergenType = 'Removable';
 							} else {
-								$scope.placeholderRow.item.type = 2;
+								row[col].allergenType = 'No allergen';
 							}
+            }
+						columnValues = null;
+						continue;
+          } else {
+            continue;
+          }
+        }
+        if (!columnValues) {
+          columnValues = {};
+          $scope.gridColumns.forEach(function(value, index) {
+            columnValues[value] = [];
+          });
+        }
+        for (var col in columnValues) {
+          columnValues[col].push(row[col].allergenType);
+        }
+      }
+    };
 
-							var rowEntry = angular.copy($scope.placeholderRow);
-							rowEntry.item.name = line;
 
-							if ($scope.grid.tableData) {
-								$scope.grid.tableData.push(rowEntry);
-							} else {
-								$scope.grid.tableData = [rowEntry];
-							}
-						});
+    $scope.newRowLogicFn = function() {
+      if (!$scope.placeholderRow.item.name) {
+        return;
+      }
+      var lineSplit = $scope.placeholderRow.item.name.split('\n');
+      lineSplit.forEach(function(line, index) {
+        line = line.trim();
+        if (line.length == 0) {
+          return;
+        }
+        if (line.charAt(line.length - 1) == ':') {
+          $scope.placeholderRow.item.type = 1;
+        } else if (line.charAt(0) == '-') {
+          line = line.substring(1, line.length);
+          $scope.placeholderRow.item.type = 3;
+        } else if (line.charAt(0) == '*') {
+          line = line.substring(1, line.length);
+          $scope.placeholderRow.item.type = 3;
+        } else {
+          $scope.placeholderRow.item.type = 2;
+        }
 
-						$scope.placeholderRow = angular.copy($scope.resetRow);
+        var rowEntry = angular.copy($scope.placeholderRow);
+        rowEntry.item.name = line;
 
-						$scope.saveGridFn();
+        if ($scope.grid.tableData) {
+          $scope.grid.tableData.push(rowEntry);
+        } else {
+          $scope.grid.tableData = [rowEntry];
+        }
+      });
 
-						console.info('New grid entry >>>', $scope.grid.tableData);
-				};
+      $scope.placeholderRow = angular.copy($scope.resetRow);
 
-				$scope.addNewRowFn = function($event) {
-					if ($event.keyCode == 13 || $event.keyCode == 10) {
-						if ($event.ctrlKey) {
-							$scope.placeholderRow.item.name += '\n';
-						} else {
-							$event.preventDefault();
-							$scope.newRowLogicFn();
-						}
-					}
-				};
+			$scope.updateSectionsFn();
 
-				$scope.editCellFn = function(cell) {
-					$scope.selectedEditCell = cell;
-					$('#cellEditModal').modal('show');
-				};
+      $scope.saveGridFn();
 
-				$scope.toggleNoteFn = function() {
-					if ($scope.placeholderRow.item.hasNote) {
-						$scope.placeholderRow.item.hasNote = false;
-					} else {
-						$scope.placeholderRow.item.hasNote = true;
-					}
-				};
+      console.info('New grid entry >>>', $scope.grid.tableData);
+    };
 
-				$scope.togglePriceFn = function() {
-					if ($scope.placeholderRow.item.hasPrice) {
-						$scope.placeholderRow.item.hasPrice = false;
-					} else {
-						$scope.placeholderRow.item.hasPrice = true;
-					}
-				};
+    $scope.addNewRowFn = function($event) {
+      if ($event.keyCode == 13 || $event.keyCode == 10) {
+        if ($event.ctrlKey) {
+          $scope.placeholderRow.item.name += '\n';
+        } else {
+          $event.preventDefault();
+          $scope.newRowLogicFn();
+        }
+      }
+    };
 
-				$scope.toggleCharacteristicFn = function() {
-					if ($scope.placeholderRow.item.hasCharacteristic) {
-						$scope.placeholderRow.item.hasCharacteristic = false;
-					} else {
-						$scope.placeholderRow.item.hasCharacteristic = true;
-					}
-				};
+    $scope.editCellFn = function(cell) {
+      $scope.selectedEditCell = cell;
+      $('#cellEditModal').modal('show');
+    };
 
-				$scope.toggleVeggieEditFn = function(veggie) {
-					if (veggie.edit) {
-						veggie.edit = false;
-					} else {
-						veggie.edit = true;
-					}
-				};
+    $scope.toggleNoteFn = function() {
+      if ($scope.placeholderRow.item.hasNote) {
+        $scope.placeholderRow.item.hasNote = false;
+      } else {
+        $scope.placeholderRow.item.hasNote = true;
+      }
+    };
 
-				$scope.characterList = ['MSC 5', 'MSC 4', 'MSC 3', 'MSC 2', 'MSC 1', 'Organic', 'Local 30m/50km', 'Local 50m/80km', 'Local 100m/160km', 'Fairtrade'];
+    $scope.togglePriceFn = function() {
+      if ($scope.placeholderRow.item.hasPrice) {
+        $scope.placeholderRow.item.hasPrice = false;
+      } else {
+        $scope.placeholderRow.item.hasPrice = true;
+      }
+    };
 
-				$scope.loadCharTagFn = function($query) {
-					var retList = [];
-					var regex = new RegExp('^' + $query, 'i');
-					$scope.characterList.forEach(function(value, index) {
-						if (regex.test(value)) {
-							retList.push({text: value});
-						}
-					});
-					return retList;
-				};
+    $scope.toggleCharacteristicFn = function() {
+      if ($scope.placeholderRow.item.hasCharacteristic) {
+        $scope.placeholderRow.item.hasCharacteristic = false;
+      } else {
+        $scope.placeholderRow.item.hasCharacteristic = true;
+      }
+    };
 
-				$scope.addToCharFn = function(val) {
-					$scope.placeholderRow.item.characteristic.push({text: val});
-				};
+    $scope.toggleVeggieEditFn = function(veggie) {
+      if (veggie.edit) {
+        veggie.edit = false;
+      } else {
+        veggie.edit = true;
+      }
+    };
 
-				$scope.checkIfCharAddedFn = function(val) {
-					var retStatus = false;
-					if (!$scope.placeholderRow.item.characteristic) {
-						return retStatus;
-					}
+    $scope.characterList = ['MSC 5', 'MSC 4', 'MSC 3', 'MSC 2', 'MSC 1', 'Organic', 'Local 30m/50km', 'Local 50m/80km', 'Local 100m/160km', 'Fairtrade'];
 
-					$scope.placeholderRow.item.characteristic.forEach(function(value, index) {
-						if (value.text == val) {
-							retStatus = true;
-						}
-					});
+    $scope.loadCharTagFn = function($query) {
+      var retList = [];
+      var regex = new RegExp('^' + $query, 'i');
+      $scope.characterList.forEach(function(value, index) {
+        if (regex.test(value)) {
+          retList.push({
+            text: value
+          });
+        }
+      });
+      return retList;
+    };
 
-					return retStatus;
-				};
+    $scope.addToCharFn = function(val) {
+      $scope.placeholderRow.item.characteristic.push({
+        text: val
+      });
+    };
 
-				$scope.toggleItemEditFn = function(item) {
-					if (item.isEdit) {
-						item.isEdit = false;
-					} else {
-						item.isEdit = true;
-					}
-				};
+    $scope.checkIfCharAddedFn = function(val) {
+      var retStatus = false;
+      if (!$scope.placeholderRow.item.characteristic) {
+        return retStatus;
+      }
 
-				$scope.saveItemEditFn = function($event, $index, item) {
-					if ($event.keyCode == 13 || $event.keyCode == 10) {
-						$scope.toggleItemEditFn(item);
-						if (!item.name) {
-							$scope.grid.tableData.splice($index, 1);
-						} else if (item.name.length == 0) {
-							$scope.grid.tableData.splice($index, 1);
-						} else {
-							$scope.grid.tableData.splice($index + 1, 0, angular.copy($scope.resetRow));
-							$scope.grid.tableData[$index + 1].item.isEdit = true;
-						}
-						$scope.saveGridFn();
-					}
-				};
+      $scope.placeholderRow.item.characteristic.forEach(function(value, index) {
+        if (value.text == val) {
+          retStatus = true;
+        }
+      });
 
-				$scope.deleteRowFn = function($index) {
-					$scope.grid.tableData.splice($index, 1);
-					$scope.saveGridFn();
-				};
+      return retStatus;
+    };
 
-				$scope.charEditStatus = {};
+    $scope.toggleItemEditFn = function(item) {
+      if (item.isEdit) {
+        item.isEdit = false;
+      } else {
+        item.isEdit = true;
+      }
+    };
 
-				$scope.toggleCharEditFn = function($index) {
-					if ($scope.charEditStatus[$index]) {
-						$scope.charEditStatus[$index] = false;
-					} else {
-						$scope.charEditStatus[$index] = true;
-					}
-				};
-	}
+    $scope.saveItemEditFn = function($event, $index, item) {
+      if ($event.keyCode == 13 || $event.keyCode == 10) {
+        $scope.toggleItemEditFn(item);
+        if (!item.name) {
+          $scope.grid.tableData.splice($index, 1);
+        } else if (item.name.length == 0) {
+          $scope.grid.tableData.splice($index, 1);
+        } else {
+          $scope.grid.tableData.splice($index + 1, 0, angular.copy($scope.resetRow));
+          $scope.grid.tableData[$index + 1].item.isEdit = true;
+        }
+        $scope.saveGridFn();
+      }
+    };
+
+    $scope.deleteRowFn = function($index) {
+      $scope.grid.tableData.splice($index, 1);
+      $scope.saveGridFn();
+    };
+
+    $scope.charEditStatus = {};
+
+    $scope.toggleCharEditFn = function($index) {
+      if ($scope.charEditStatus[$index]) {
+        $scope.charEditStatus[$index] = false;
+      } else {
+        $scope.charEditStatus[$index] = true;
+      }
+    };
+  }
 ]);
