45c81220609fee1132cb3085b66bb3ffc75480e0
Jaideep Das <jaideep.jdof@gmail.com>
2015-06-12 23:40:51 +0530

    bug "make 'required' allergen inputs required on new row line, too"

--- public/modules/grids/directives/cell-edit-new.client.directive.js
+++ public/modules/grids/directives/cell-edit-new.client.directive.js
@@ -1,56 +1,74 @@
 'use strict';
 
 angular.module('grids').directive('cellEditNew', ['$compile',
-	function($compile) {
-		return {
-			templateUrl: 'modules/grids/views/cell-edit-new.client.partial.html',
-			restrict: 'E',
-            scope: {
-              columnName: '=columnName'
-            },
-			link: function postLink(scope, element, attrs) {
-              scope.cell = scope.$parent.placeholderRow[scope.columnName];
-              scope.placeholderCell = angular.copy(scope.cell);
-			  scope.$watch('$parent.placeholderRow', function(newValue, oldValue) {
-				scope.cell = scope.$parent.placeholderRow[scope.columnName];
-				scope.placeholderCell = angular.copy(scope.cell);
-			  });
-              $(element).popover({
-                html: true,
-                title: false,
-                placement: 'bottom',
-                content: function() {
-                  return $compile($('.cell-new-' + scope.columnName).html())(scope);
-                }
-              });
+  function($compile) {
+    return {
+      templateUrl: 'modules/grids/views/cell-edit-new.client.partial.html',
+      restrict: 'E',
+      scope: {
+        columnName: '=columnName'
+      },
+      link: function postLink(scope, element, attrs) {
+        scope.cell = scope.$parent.placeholderRow[scope.columnName];
+        scope.placeholderCell = angular.copy(scope.cell);
+        scope.$watch('$parent.placeholderRow', function(newValue, oldValue) {
+          scope.cell = scope.$parent.placeholderRow[scope.columnName];
+          scope.placeholderCell = angular.copy(scope.cell);
+        });
+        $(element).popover({
+          html: true,
+          title: false,
+          placement: 'bottom',
+          content: function() {
+            return $compile($('.cell-new-' + scope.columnName).html())(scope);
+          }
+        });
 
-              scope.inputDismissFn = function($event) {
-                if ($event.keyCode == 13) {
-                  scope.saveCellEditFn();
-                }
-              };
+        scope.inputDismissFn = function($event) {
+          if ($event.keyCode == 13) {
+						if (scope.specialColumnAllergenReqFn() == false) {
+            	scope.saveCellEditFn();
+						}
+          }
+        };
 
-              scope.saveCellEditFn = function() {
-                scope.cell = angular.copy(scope.placeholderCell);
-                scope.placeholderCell = angular.copy(scope.cell);
-                scope.$parent.updatePlaceholderRowCellFn(scope.columnName, scope.placeholderCell);
-                $(element).popover('hide');
-              };
+        scope.saveCellEditFn = function() {
+          scope.cell = angular.copy(scope.placeholderCell);
+          scope.placeholderCell = angular.copy(scope.cell);
+          scope.$parent.updatePlaceholderRowCellFn(scope.columnName, scope.placeholderCell);
+          $(element).popover('hide');
+        };
 
-              scope.cancelCellEditFn = function() {
-                $(element).popover('hide');
-              };
+        scope.cancelCellEditFn = function() {
+          $(element).popover('hide');
+        };
 
-              scope.$watch('placeholderCell.allergenType', function(newValue, oldValue) {
-								if(newValue == oldValue) {
-									return;
-								}
-								console.log('cell allergy update');
-                if (newValue == 'No allergen') {
-                  scope.saveCellEditFn();
-                }
-              });
-			}
-		};
-	}
+        scope.$watch('placeholderCell.allergenType', function(newValue, oldValue) {
+          if (newValue == oldValue) {
+            return;
+          }
+          console.log('cell allergy update');
+          if (newValue == 'No allergen') {
+            scope.saveCellEditFn();
+          }
+        });
+
+				scope.specialColumnAllergenReqFn = function() {
+					if (['cereal', 'crustacean', 'mollusc', 'nut'].indexOf(scope.columnName) == -1) {
+						return false;
+					} else {
+						if (['Removable', 'Fixed'].indexOf(scope.placeholderCell.allergenType) == -1) {
+							return false;
+						} else {
+							if (scope.placeholderCell.allergens && scope.placeholderCell.allergens.length) {
+								return false;
+							} else {
+								return true;
+							}
+						}
+					}
+				};
+      }
+    };
+  }
 ]);
