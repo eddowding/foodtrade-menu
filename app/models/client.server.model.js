6470909f52993fe9352375612b843b09be2d7398
Jaideep Das <jaideep.jdof@gmail.com>
2015-06-16 00:47:36 +0530

    added client model

---
+++ app/models/client.server.model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Client Schema
+ */
+var ClientSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Client name',
+		trim: true
+	},
+	domain: {
+		type: String,
+		required: true
+	},
+	logo: {
+		type: Buffer,
+		required: false
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Client', ClientSchema);
